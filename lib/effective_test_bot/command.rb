d35696465119b13ea946e30495cd94c535d205bc
Matt Riemer <matthew@agilestyle.com>
2015-01-21 14:30:38 -0700

    debugging info

--- lib/effective_test_bot/command.rb
+++ lib/effective_test_bot/command.rb
@@ -10,6 +10,9 @@ module EffectiveTestBot
     def start
       Dir.mkdir(@options[:pid_dir]) unless File.exists?(@options[:pid_dir])
 
+      # Also check how this process was called.  Don't run on delayed_job kind athing
+      puts "EffectiveTestBot started with parent process #{Process.argv0}"
+
       # Only run the EffectiveTestBot if we are running as a server
       if ['rails', 'ruby_executable_hooks'].none? { |cmd| Process.argv0.end_with?(cmd) }
         puts 'EffectiveTestBot skipping run.  Exitting.'
@@ -23,9 +26,6 @@ module EffectiveTestBot
         puts "EffectiveTestBot already run.  Exitting."
         exit 1
       end
-
-      # Also check how this process was called.  Don't run on delayed_job kind athing
-      puts "EffectiveTestBot started with parent process #{Process.argv0}"
 
       at_exit do
         (File.delete(@options[:pid_file]) rescue true)
