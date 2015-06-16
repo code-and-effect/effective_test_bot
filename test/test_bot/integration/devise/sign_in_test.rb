8fa288cd00a59040d7b2dd22ebf23fbac238db21
Matt Riemer <matthew@agilestyle.com>
2015-06-16 14:00:21 -0600

    Remove commented out code

--- test/test_bot/integration/devise/sign_in_test.rb
+++ test/test_bot/integration/devise/sign_in_test.rb
@@ -8,8 +8,6 @@ if defined?(Devise) && defined?(User)
       test 'valid sign in' do
         visit new_user_session_path
 
-        binding.pry
-
         within('form#new_user') do
           fill_in 'user_email', with: user.email
           fill_in 'user_password', with: 'password'
@@ -39,46 +37,3 @@ if defined?(Devise) && defined?(User)
     end
   end
 end
-
-# require 'rails_helper'
-
-# if defined?(Devise) && defined?(User)
-
-#   feature 'Devise Sign In' do
-#     let(:user) do
-#       User.new(email: "user_#{Time.now.to_i}@example.com", password: '1234567890').tap { |u| u.save(validate: false) }
-#     end
-
-#     it 'allows a valid sign in' do
-#       visit new_user_session_path
-
-#       within('form#new_user') do
-#         fill_in 'user_email', with: user.email
-#         fill_in 'user_password', with: user.password
-
-#         find('input[type=submit]').click
-#       end
-
-#       expect(page.status_code).to eq 200
-#       expect(page).to have_content I18n.t('devise.sessions.signed_in')
-
-#       existing_user = User.find_by_email(user.email)
-#       expect(existing_user.sign_in_count).to eq 1
-
-#     it 'prevents sign in when provided an invalid password' do
-#       visit new_user_session_path
-
-#       within('form#new_user') do
-#         fill_in 'user_email', with: user.email
-#         fill_in 'user_password', with: 'invalid_password'
-
-#         find('input[type=submit]').click
-#       end
-
-#       expect(page.status_code).to eq 200
-#       expect(page).to have_content I18n.t('devise.failure.invalid', authentication_keys: Devise.authentication_keys.join(', '))
-#     end
-
-#   end
-
-# end # /defined?(Devise)
--- test/test_bot/integration/home_page_test.rb
+++ test/test_bot/integration/home_page_test.rb
@@ -6,23 +6,5 @@ module TestBot
       visit root_path
       assert_equal page.status_code, 200
     end
-
-
-    # let(:something) { User.new(:email => 'homepagetest@someone.com') }
-
-    # test "sanity" do
-    #   visit root_path
-    #   page.save_screenshot('something.png')
-
-    #   assert_equal current_path, 'http://something.com', 'unexpected root_url'
-
-    #   assert_content "Testbot home page spec!"
-    # end
-
-    # test "does something" do
-    #   visit root_path
-    #   page.must_have_content "Test bot home page spec!"
-    # end
-
   end
 end
