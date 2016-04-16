76976d208a2d880582330b3243d2829d76ee6069
Matt Riemer <matthew@agilestyle.com>
2016-04-16 12:57:48 -0600

    Assert assigns_errors, exceptions, page status on submit

--- test/support/effective_test_bot_form_helper.rb
+++ test/support/effective_test_bot_form_helper.rb
@@ -29,6 +29,10 @@ module EffectiveTestBotFormHelper
     end
 
     assert_no_unpermitted_params unless test_bot_skip?(:no_unpermitted_params)
+    assert_no_assigns_errors unless test_bot_skip?(:no_assigns_errors)
+    assert_no_exceptions unless test_bot_skip?(:exceptions)
+    assert_page_status unless test_bot_skip?(:page_status)
+
     true
   end
 
