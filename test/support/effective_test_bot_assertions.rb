c1e97db9ace2cad814c814b39807d4b698f0d9cc
Matt Riemer <matthew@agilestyle.com>
2017-11-10 15:25:13 -0700

    another finder fix

--- test/support/effective_test_bot_assertions.rb
+++ test/support/effective_test_bot_assertions.rb
@@ -128,7 +128,7 @@ module EffectiveTestBotAssertions
   # Rails jquery-ujs data-disable-with
   # = f.button :submit, 'Save', data: { disable_with: 'Saving...' }
   def assert_jquery_ujs_disable_with(label = nil, message = nil)
-    submits = label.present? ? [find(:link_or_button, label)] : all("input[type='submit']")
+    submits = label.present? ? all("input[type='submit']", text: label) : all("input[type='submit']")
     all_disabled_with = submits.all? { |submit| submit['data-disable-with'].present? }
 
     assert all_disabled_with, message || "(jquery_ujs_disable_with) Expected rails jquery-ujs data-disable-with to be present on #{(label || "all input[type='submit'] fields")}\nInclude it on your submit buttons by adding \"data: { disable_with: 'Saving...' }\""
