1adf98637d4b43fb431ed04de103787dc1073271
Matt Riemer <matthew@agilestyle.com>
2018-06-25 10:46:33 -0600

    better test helper

--- config/test_helper.rb
+++ config/test_helper.rb
@@ -1,19 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
-require 'rake'
 require 'rails/test_help'
-
 require 'minitest/spec'
 require 'minitest/reporters'
 require 'minitest/fail_fast' if EffectiveTestBot.fail_fast?
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
-
-  # For the let syntax
-  extend Minitest::Spec::DSL
+  fixtures :all               # Loads all fixtures in test/fixtures/*.yml
+  extend Minitest::Spec::DSL  # For the let syntax
 end
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
@@ -22,27 +17,13 @@ Rails.backtrace_cleaner.remove_silencers!
 Rails.backtrace_cleaner.add_silencer { |line| line =~ /minitest/ }
 #Rails.backtrace_cleaner.add_silencer { |line| line =~ /effective_test_bot/ }
 
-###############################################
-### Effective Test Bot specific stuff below ###
-###############################################
+setup = ['db:schema:load', 'db:fixtures:load', 'db:seed', 'test:load_fixture_seeds'].join(' ')
+system("rails #{setup} RAILS_ENV=test")
 
-Rails.application.load_tasks
+# rails test
+# rails test:system
+# rails test:bot:environment
+# rails test:bot
 
-# So the very first thing we do is consistently reset the database.
-# This can be done with Snippet 1 or Snippet 2.
-# Snippet 1 is faster, and will usually work. Snippet 2 should always work.
-
-# Snippet 1:
-Rake::Task['db:schema:load'].invoke
-ActiveRecord::Migration.maintain_test_schema!
-
-# Snippet 2:
-
-# Rake::Task['db:drop'].invoke
-# Rake::Task['db:create'].invoke
-# Rake::Task['db:migrate'].invoke
-
-# Now we populate our test data:
-Rake::Task['db:fixtures:load'].invoke
-Rake::Task['db:seed'].invoke
-Rake::Task['test:load_fixture_seeds'].invoke # from effective_test_bot. Loads test/fixtures/seeds.rb
+# rails test:system TOUR=true
+# rails test:bot TEST=posts#index
