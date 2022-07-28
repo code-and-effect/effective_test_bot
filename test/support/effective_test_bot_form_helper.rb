cc61422b11187bbb2413e23c508386be7b7a83bd
Matt Riemer <matthew@codeandeffect.com>
2022-07-28 13:40:45 -0600

    do assign errors first

--- test/support/effective_test_bot_form_helper.rb
+++ test/support/effective_test_bot_form_helper.rb
@@ -35,10 +35,11 @@ module EffectiveTestBotFormHelper
       with_raised_unpermitted_params_exceptions { click_submit(label, last: last, debug: debug) }
     end
 
+    assert_no_assigns_errors unless test_bot_skip?(:no_assigns_errors)
+
     # This is a blocking selector that will wait until the page has changed url
     assert_no_current_path(before_path)
 
-    assert_no_assigns_errors unless test_bot_skip?(:no_assigns_errors)
     assert_no_exceptions unless test_bot_skip?(:exceptions)
     assert_authorization unless test_bot_skip?(:authorization)
     assert_page_status unless test_bot_skip?(:page_status)
